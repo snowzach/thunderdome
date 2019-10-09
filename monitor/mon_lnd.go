cc83d73e40118213c98981d67c80ca542227ba85
Zach Brown <zach.brown@coinninja.com>
2019-10-09 19:16:49 +0000

    Remove cap for non DropBit lightning payments, Stats update.

--- monitor/mon_lnd.go
+++ monitor/mon_lnd.go
@@ -6,34 +6,19 @@ import (
 
 	"git.coinninja.net/backend/thunderdome/conf"
 	"github.com/lightningnetwork/lnd/lnrpc"
-	"go.opencensus.io/stats"
-	"go.opencensus.io/stats/view"
 	"go.uber.org/zap"
 )
 
 type LNDStats struct {
-	ConfirmedBalance int64 `json:"confirmed_balance"`
-	LocalBalance     int64 `json:"local_balance"`
-	RemoteBalance    int64 `json:"remote_balance"`
-	ChannelCount     int   `json:"channel_count"`
+	TotalBalance       int64 `json:"total_balance"`
+	ConfirmedBalance   int64 `json:"confirmed_balance"`
+	UnconfirmedBalance int64 `json:"uncomfirmed_balance"`
+	LocalBalance       int64 `json:"local_balance"`
+	RemoteBalance      int64 `json:"remote_balance"`
+	ChannelCount       int   `json:"channel_count"`
 }
 
 func (m *Monitor) MonitorLND() {
-
-	confirmedBalance := stats.Int64("lnd/confirmed_balance", "The confirmed LND wallet balance", stats.UnitDimensionless)
-	localBalance := stats.Int64("lnd/local_balance", "The total local balance of the LND wallet on all channels", stats.UnitDimensionless)
-	remoteBalance := stats.Int64("lnd/remote_balance", "The total remote balance of the LND wallet on all channels", stats.UnitDimensionless)
-	openChannels := stats.Int64("lnd/open_channels", "The count of open channels", stats.UnitDimensionless)
-
-	// Register the views
-	if err := view.Register(
-		&view.View{Name: confirmedBalance.Name(), Measure: confirmedBalance, Description: confirmedBalance.Description(), Aggregation: view.LastValue()},
-		&view.View{Name: localBalance.Name(), Measure: localBalance, Description: localBalance.Description(), Aggregation: view.LastValue()},
-		&view.View{Name: remoteBalance.Name(), Measure: remoteBalance, Description: remoteBalance.Description(), Aggregation: view.LastValue()},
-		&view.View{Name: openChannels.Name(), Measure: openChannels, Description: openChannels.Description(), Aggregation: view.LastValue()},
-	); err != nil {
-		m.logger.Errorf("Could not register monitor views: %v", err)
-	}
 
 	// Handle shutting down
 	ctx, cancel := context.WithCancel(context.Background())
@@ -64,7 +49,9 @@ monLoop:
 			m.logger.Errorw("Could not get wallet balance", "error", err)
 			continue
 		}
+		stats.TotalBalance = walletBalanceResponse.TotalBalance
 		stats.ConfirmedBalance = walletBalanceResponse.ConfirmedBalance
+		stats.UnconfirmedBalance = walletBalanceResponse.UnconfirmedBalance
 
 		// List our channels
 		listChannelsResponse, err := m.lclient.ListChannels(ctx, &lnrpc.ListChannelsRequest{ActiveOnly: true, PublicOnly: true})
@@ -82,7 +69,9 @@ monLoop:
 		m.logger.Debugw("LND Stats", zap.Any("lnd_stats", stats))
 
 		if m.ddclient != nil {
+			m.ddclient.Gauge("lnd.total_balance", float64(stats.TotalBalance), []string{}, 1)
 			m.ddclient.Gauge("lnd.confirmed_balance", float64(stats.ConfirmedBalance), []string{}, 1)
+			m.ddclient.Gauge("lnd.unconfirmed_balance", float64(stats.UnconfirmedBalance), []string{}, 1)
 			m.ddclient.Gauge("lnd.local_balance", float64(stats.LocalBalance), []string{}, 1)
 			m.ddclient.Gauge("lnd.remote_balance", float64(stats.RemoteBalance), []string{}, 1)
 			m.ddclient.Gauge("lnd.channel_count", float64(stats.ChannelCount), []string{}, 1)
